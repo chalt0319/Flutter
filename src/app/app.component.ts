d1ac537765c832bbc5d6d7d734fc1e92ae1d1418
Courtney <chalt0319@gmail.com>
2018-11-13 10:13:01 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  postArray=[]
+
+  addPost(value, image, link){
+    let hash = {text: value, likes: 0, image: image, link: link, shortLink: link.slice(0, 50) + "...", comments: [], boolean: false}
+    this.postArray.push(hash)
+  }
+
+  likePost(post){
+
+    this.postArray.map(function(thePost) {
+      if (thePost === post){
+        return thePost.likes += 1
+      }
+    })
+  }
+
+  addComment(post, comment){
+    this.postArray.map(function(thePost) {
+      if (thePost === post){
+        return thePost.comments.push(comment)
+      }
+    })
+  }
+
+
+}
