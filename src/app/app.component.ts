365aed44263e811adda50cdb19cab82b3979ed17
Courtney <chalt0319@gmail.com>
2018-11-17 15:42:08 -0500

    remove all debugging code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   postArray=[]
-  index=0
 
   addPost(value, image, link){
-    let hash = {index: this.index,text: value, likes: 0, image: image, link: link, shortLink: link.slice(0, 50) + "...", comments: [], boolean: false}
-    this.postArray.push(hash)
-    this.index += 1
+    let hash = {text: value, likes: 0, image: image, link: link, shortLink: link.slice(0, 50) + "...", comments: [], boolean: false}
+    this.postArray.unshift(hash)
   }
 
   likePost(post){
